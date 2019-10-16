80cb9aae98e8af9ab030a87f08754715159618c5
Jan Bednařík <jan.bednarik@gmail.com>
2019-10-16 11:04:27 +0200

    Powerline config

---
+++ powerline-shell/theme.py
@@ -0,0 +1,19 @@
+from powerline_shell.themes.default import DefaultColor
+
+
+class Color(DefaultColor):
+    USERNAME_BG = 234
+    USERNAME_FG = 15
+    USERNAME_ROOT_BG = 1
+
+    PATH_BG = 235
+    PATH_FG = 15
+    CWD_FG = 15
+
+    REPO_CLEAN_BG = 234
+    REPO_CLEAN_FG = 2
+    REPO_DIRTY_BG = 234
+    REPO_DIRTY_FG = 1
+
+    VIRTUAL_ENV_BG = 232
+    VIRTUAL_ENV_FG = 228
